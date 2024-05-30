41a142de2326e8dc2103ff7e04e8a1d329c2be97
ccm1cM <ccm171m@gmail.com>
2024-05-30 09:48:53 -0700

    Actualizacion1

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    //Definimos la estructura de nuestro board
+    public int width;
+    public int height;
+    public GameObject tileObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        SetupBoard();
+    
+    }
+
+    private void SetupBoard() 
+    {
+        for (int x = 0; x < width; x++) 
+        {
+            for(int y = 0; y < height; y++) 
+            {
+                var o = Instantiate(tileObject, new Vector3 (x, y, -5), Quaternion.identity);
+                o.transform.parent = transform ;
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
